9670c67666f69bf97beb1147c64527fd91ee8abb
CharlesEmmanuel <charlesemmanuel@pm.me>
2020-03-30 00:20:48 +0000

    update contact page

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -8,6 +8,7 @@
                 <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus voluptates harum culpa laboriosam, nulla tenetur cupiditate quo at saepe iste sed facere fugit, laudantium est error sit, debitis, vitae libero!</p>
                     <h1>Contact Page</h1>
                     <p>Mon Adresse email : charlesemmanuel@pm.me</p>
                 </div>
